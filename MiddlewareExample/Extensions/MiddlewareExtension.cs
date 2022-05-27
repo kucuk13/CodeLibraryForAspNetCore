1c39ed5220d20469424b54595f591e6615ecbdbe
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2022-05-27 23:36:50 +0300

    Custom Middleware Added

---
+++ MiddlewareExample/Extensions/MiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using MiddlewareExample.Middlewares;
+
+namespace MiddlewareExample.Extensions
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomMiddeleware(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
---
+++ MiddlewareExample/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace MiddlewareExample.Middlewares
+{
+    public class CustomMiddleware
+    {
+        RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var watch = Stopwatch.StartNew();
+            await _next.Invoke(context);
+            watch.Stop();
+            Console.WriteLine(context.Request.Scheme + 
+                "://" + 
+                context.Request.Host.Value + 
+                context.Request.Path.Value + 
+                " service executed in " + 
+                watch.ElapsedMilliseconds + " ms.");
+        }
+    }
+}
